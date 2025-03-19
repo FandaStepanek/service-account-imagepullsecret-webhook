553752f89e1dfc14299105fb9ee0d3e3c66deef0
FandaStepanek <frantisek.stepanek@newtontech.cz>
2025-03-19 13:41:18 +0100

    Add logging

--- webhook.py
+++ webhook.py
@@ -3,8 +3,10 @@ import json
 import base64
 import os
 import requests
+import logging
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO) 
 
 # Kubernetes API Config
 K8S_API_SERVER = os.getenv("K8S_API_SERVER", "https://kubernetes.default.svc")
@@ -13,7 +15,7 @@ K8S_CA_PATH = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 
 # Secret Config
 DOCKERHUB_SECRET_NAME = "dockerhub-secret"
-DEFAULT_NAMESPACE = "default"
+DEFAULT_NAMESPACE = os.getenv("K8S_API_SERVER", "default")
 
 def get_k8s_token():
     """ Reads Kubernetes service account token for API authentication """
@@ -33,16 +35,18 @@ def copy_secret_to_namespace(namespace):
     # Get secret from default namespace
     url_get = f"{K8S_API_SERVER}/api/v1/namespaces/{DEFAULT_NAMESPACE}/secrets/{DOCKERHUB_SECRET_NAME}"
     url_create = f"{K8S_API_SERVER}/api/v1/namespaces/{namespace}/secrets"
-
+    token = get_k8s_token()
+    app.logger.info(f"Using token: {token}")
     headers = {
-        "Authorization": f"Bearer {get_k8s_token()}",
+        "Authorization": f"Bearer {token}",
         "Content-Type": "application/json"
     }
 
     response = requests.get(url_get, headers=headers, verify=K8S_CA_PATH)
     
+    app.logger.info(f"Response: {response}")
     if response.status_code != 200:
-        print(f" ERROR: Failed to get `{DOCKERHUB_SECRET_NAME}` from `{DEFAULT_NAMESPACE}`")
+        app.logger.info(f" ERROR: Failed to get `{DOCKERHUB_SECRET_NAME}` from `{DEFAULT_NAMESPACE}`")
         return False
     
     secret_data = response.json()
@@ -55,10 +59,10 @@ def copy_secret_to_namespace(namespace):
     response = requests.post(url_create, headers=headers, json=secret_data, verify=K8S_CA_PATH)
     
     if response.status_code == 201:
-        print(f" Copied `{DOCKERHUB_SECRET_NAME}` to `{namespace}`")
+        app.logger.info(f" Copied `{DOCKERHUB_SECRET_NAME}` to `{namespace}`")
         return True
     else:
-        print(f" ERROR: Failed to create `{DOCKERHUB_SECRET_NAME}` in `{namespace}`: {response.text}")
+        app.logger.error(f" ERROR: Failed to create `{DOCKERHUB_SECRET_NAME}` in `{namespace}`: {response.text}")
         return False
 
 @app.route("/mutate", methods=["POST"])
@@ -77,6 +81,7 @@ def mutate():
         patches = []
 
         # Ensure `dockerhub-secret` exists in the namespace
+        app.logger.info(f"Inspecting namespace: {namespace}")
         if not check_secret_exists(namespace):
             copy_secret_to_namespace(namespace)
 
